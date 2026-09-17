0271d8cfbf7511a2d4369da5e35e8c626ed4ed4a
aMIr <94835248+arthur0010@users.noreply.github.com>
2026-09-17 12:22:53 +0330

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ RENDER_URL = os.environ.get("RENDER_EXTERNAL_URL", "")
 TELEGRAM_API = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}"
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 flask_app = Flask(__name__)
 
