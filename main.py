d91c1868126640a843273d224b71fb3e47ad4d6b
aMIr <94835248+arthur0010@users.noreply.github.com>
2026-09-17 12:45:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def ask_gemini(user_text):
         )
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.1-flash-lite",
             contents=prompt,
             config=types.GenerateContentConfig(
                 automatic_function_calling=types.AutomaticFunctionCallingConfig(
